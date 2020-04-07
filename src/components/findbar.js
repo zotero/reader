489f1257219ecb372dae6d0274f3862c055cf720
Flachware <mail@flachware.com>
2020-04-07 17:42:52 +0200

    #14 Style close button.

--- src/components/findbar.js
+++ src/components/findbar.js
@@ -7,19 +7,20 @@ class Findbar extends React.Component {
   getContainerNode() {
     return document.getElementById('findbar');
   }
-  
+
   handleClose() {
     PDFViewerApplication.findBar.close();
   }
-  
+
   render() {
     let containerNode = this.getContainerNode();
     return ReactDOM.createPortal(
       <React.Fragment>
-        <div>
+        <div className="close-container">
           <button
-            className="toolbarButton findClose"
+            className="close"
             onClick={this.handleClose}
+            tabIndex={-1}
           />
         </div>
       </React.Fragment>,
