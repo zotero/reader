8ca4d95f9f1fdfde37d6a54379c302b11480fd2f
Flachware <mail@flachware.com>
2020-05-26 18:30:09 +0200

    #35 Remove copy and highlight button. Remove popupbutton assets. Style selection menu.

--- src/components/selection-menu.js
+++ src/components/selection-menu.js
@@ -8,26 +8,34 @@ class SelectionMenu extends React.Component {
   }
 
   render() {
-    let { onCopy, onHighlight, color } = this.props;
+    let { color } = this.props;
     return (
       <div className="selection-menu">
-        <button className="toolbarButton btn-copy" onClick={onCopy} data-l10n-id="copy">
-          <span data-l10n-id="copy_label">Copy</span>
-        </button>
-        <div className="tool-group selection-tools">
-          <button className="toolbarButton btn-highlight" onClick={onHighlight} data-l10n-id="highlight">
-            <span data-l10n-id="highlight_label">Highlight</span>
-          </button>
-          <button
-            data-l10n-id='global_color'
-            className="toolbarButton global-color"
-            style={{ color }}
-            onClick={this.handleColorPick}
-          >
-            <span data-l10n-id="global_color_label">Global Color</span>
-            <span className="dropmarker"></span>
-          </button>
-        </div>
+        <button
+          className="toolbarButton global-color"
+          style={{ color }}
+          onClick={this.handleColorPick}
+        />
+        <button
+          className="toolbarButton global-color"
+          style={{ color }}
+          onClick={this.handleColorPick}
+        />
+        <button
+          className="toolbarButton global-color"
+          style={{ color }}
+          onClick={this.handleColorPick}
+        />
+        <button
+          className="toolbarButton global-color"
+          style={{ color }}
+          onClick={this.handleColorPick}
+        />
+        <button
+          className="toolbarButton global-color"
+          style={{ color }}
+          onClick={this.handleColorPick}
+        />
       </div>
     );
   }
