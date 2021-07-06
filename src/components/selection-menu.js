1a678ffb5cbfd949a015d1fd25a825643a0bf632
Martynas Bagdonas <martbgd@gmail.com>
2021-07-06 15:31:53 +0300

    Localize color names

--- src/components/selection-menu.js
+++ src/components/selection-menu.js
@@ -1,36 +1,33 @@
 'use strict';
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { annotationColors } from '../lib/colors';
 
-class SelectionMenu extends React.Component {
-	handleColorPick = (color) => {
-		this.props.onHighlight(color);
+function SelectionMenu(props) {
+	const intl = useIntl();
+
+	function handleColorPick(color) {
+		props.onHighlight(color);
 	}
 
-	handleAddToNote = (event) => {
-
-	}
-
-	render() {
-		return (
-			<div className="selection-menu">
-				<div className="colors">
-					{annotationColors.map((color, index) => (<button
-						key={index}
-						className="toolbarButton global-color"
-						style={{ color: color[1] }}
-						onClick={() => this.handleColorPick(color[1])}
-					/>))}
-				</div>
-				{this.props.enableAddToNote &&
-				<div className="wide-button" onClick={this.props.onAddToNote}>
-					<FormattedMessage id="pdfReader.addToNote"/>
-				</div>}
+	return (
+		<div className="selection-menu">
+			<div className="colors">
+				{annotationColors.map((color, index) => (<button
+					key={index}
+					className="toolbarButton global-color"
+					style={{ color: color[1] }}
+					title={intl.formatMessage({ id: color[0] })}
+					onClick={() => handleColorPick(color[1])}
+				/>))}
 			</div>
-		);
-	}
+			{props.enableAddToNote &&
+			<div className="wide-button" onClick={props.onAddToNote}>
+				<FormattedMessage id="pdfReader.addToNote"/>
+			</div>}
+		</div>
+	);
 }
 
 export default SelectionMenu;
--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,11 +1,11 @@
 'use strict';
 
 export let annotationColors = [
-	['Yellow', '#ffd400'],
-	['Red', '#ff6666'],
-	['Green', '#5fb236'],
-	['Blue', '#2ea8e5'],
-	['Purple', '#a28ae5']
+	['general.yellow', '#ffd400'],
+	['general.red', '#ff6666'],
+	['general.green', '#5fb236'],
+	['general.blue', '#2ea8e5'],
+	['general.purple', '#a28ae5']
 ];
 
 export let selectionColor = navigator.platform.includes('Mac') ? '#71ADFD' : 'Highlight';
