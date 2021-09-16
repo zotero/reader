8f144a086e2cf4e6d21ecea9b6fb1df0433ccc3d
Martynas Bagdonas <martbgd@gmail.com>
2021-09-16 13:38:40 +0300

    Fix performance issue caused by slow .indexOf()

--- src/lib/text/page.js
+++ src/lib/text/page.js
@@ -1,8 +1,6 @@
 let isNum = c => c >= '0' && c <= '9';
 
-function getSurroundedNumber(chs, ch) {
-	let idx = chs.indexOf(ch);
-
+function getSurroundedNumber(chs, idx) {
 	while (
 		idx > 0 && isNum(chs[idx - 1].c)
 		&& Math.abs(chs[idx].rect[0] - chs[idx - 1].rect[2]) < chs[idx].rect[2] - chs[idx].rect[0]
@@ -26,10 +24,11 @@ function getSurroundedNumber(chs, ch) {
 }
 
 function getSurroundedNumberAtPos(chs, x, y) {
-	for (let ch of chs) {
+	for (let i = 0; i < chs.length; i++) {
+		let ch = chs[i];
 		let { x: x2, y: y2 } = getRectCenter(ch.rect);
 		if (isNum(ch.c) && Math.abs(x - x2) < 10 && Math.abs(y - y2) < 5) {
-			return getSurroundedNumber(chs, ch);
+			return getSurroundedNumber(chs, i);
 		}
 	}
 	return null;
@@ -52,13 +51,15 @@ export async function getPageLabelPoints(pageIndex, chs1, chs2, chs3, chs4, page
 	let chsNum3 = filterNums(chs3, pageHeight);
 	let chsNum4 = filterNums(chs4, pageHeight);
 
-	for (let ch1 of chsNum1) {
-		for (let ch3 of chsNum3) {
+	for (let c1 = 0; c1 < chsNum1; c1++) {
+		let ch1 = chsNum1[c1];
+		for (let c3 = 0; c3 < chsNum3; c3++) {
+			let ch3 = chsNum3[c3];
 			let { x: x1, y: y1 } = getRectCenter(ch1.rect);
 			let { x: x2, y: y2 } = getRectCenter(ch3.rect);
 			if (Math.abs(x1 - x2) < 10 && Math.abs(y1 - y2) < 5) {
-				let num1 = getSurroundedNumber(chs1, ch1);
-				let num3 = getSurroundedNumber(chs3, ch3);
+				let num1 = getSurroundedNumber(chs1, c1);
+				let num3 = getSurroundedNumber(chs3, c3);
 				if (num1 && num1 + 2 === num3) {
 					let pos1 = { x: x1, y: y1, num: num1, idx: pageIndex };
 
@@ -67,13 +68,15 @@ export async function getPageLabelPoints(pageIndex, chs1, chs2, chs3, chs4, page
 						return [pos1];
 					}
 
-					for (let ch2 of chsNum2) {
-						for (let ch4 of chsNum4) {
+					for (let c2 = 0; c2 < chsNum2; c2++) {
+						let ch2 = chsNum2[c2];
+						for (let c4 = 0; c4 < chsNum4; c4++) {
+							let ch4 = chsNum2[c4];
 							let { x: x1, y: y1 } = getRectCenter(ch2.rect);
 							let { x: x2, y: y2 } = getRectCenter(ch4.rect);
 							if (Math.abs(x1 - x2) < 10 && Math.abs(y1 - y2) < 5) {
-								let num2 = getSurroundedNumber(chs2, ch2);
-								let num4 = getSurroundedNumber(chs4, ch4);
+								let num2 = getSurroundedNumber(chs2, c2);
+								let num4 = getSurroundedNumber(chs4, c4);
 								if (num1 + 1 === num2 && num2 + 2 === num4) {
 									let pos2 = { x: x1, y: y1, num: num2, idx: pageIndex + 2 };
 									return [pos1, pos2];
