zotero/reader

commit 4537d7551824d1bd0d65ddd61df35430c508e468

Use annotation filter for pages view as well zotero/zotero#2391 Fixes zotero/zotero#2382

src/components/annotator.js
@@ -26,6 +26,7 @@ import {
 	clearSelection
 } from '../lib/utilities';
 import { p2v, p2v as p2vc } from '../lib/coordinates';
+import { cleanFilter, filterAnnotations } from '../lib/search';
 
 // Note: All rects in annotator.js are stored in [left, top, right, bottom] order
 // where the Y axis starts from the bottom:
@@ -482,7 +483,9 @@ const Annotator = React.forwardRef((props, ref) => {
 	// the latest value and eliminate the complexity of rebinding custom events.
 	// useRefState state variables are used only for rendering
 
+	const [_filter, filterRef, setFilter] = useRefState({ query: '', colors: [], tags: [], authors: [] });
 	const [_annotations, annotationsRef, setAnnotations] = useRefState([]);
+	const [_allAnnotations, allAnnotationsRef, setAllAnnotations] = useRefState([]);
 	const [_selectedIDs, selectedIDsRef, setSelectedIDs] = useRefState([]);
 	const [_expansionState, expansionStateRef, setExpansionState] = useRefState(0);
 	const [_mode, modeRef, setMode] = useRefState(null);
@@ -501,7 +504,6 @@ const Annotator = React.forwardRef((props, ref) => {
 	const [_enableAddToNote, enableAddToNote, setEnableAddToNote] = useRefState(false);
 	const [_labelPopup, labelPopup, setLabelPopup] = useRefState(null);
 
-	const annotationsViewRef = useRef();
 	const lastSelectedAnnotationIDRef = useRef(null);
 	const pointerDownPositionRef = useRef(null);
 	const selectionRangesRef = useRef([]);
@@ -523,12 +525,22 @@ const Annotator = React.forwardRef((props, ref) => {
 
 	useImperativeHandle(ref, () => ({
 		navigate,
-		setAnnotations,
+		setAnnotations: (annotations) => {
+			setAllAnnotations(annotations);
+			let filter = cleanFilter(annotations, filterRef.current);
+			setFilter(filter);
+			let filteredAnnotations = filterAnnotations(annotations, filter);
+			setAnnotations(filteredAnnotations);
+			if (filteredAnnotations.length !== annotationsRef.current.length) {
+				setSelectedIDs([]);
+				props.onClosePopup();
+			}
+		},
 		setColor,
 		setEnableAddToNote,
 		openPageLabelPopup,
 		editHighlightedText,
-		clearSelector: annotationsViewRef.current.clearSelector,
+		clearFilter: () => setFilter({ query: '', colors: [], tags: [], authors: []}),
 		tabToolbar: (reverse) => focusManagerRef.current.tabToolbar(reverse),
 		focusFirst: () => focusManagerRef.current.focusFirst()
 	}));
@@ -898,10 +910,16 @@ const Annotator = React.forwardRef((props, ref) => {
 					}
 				}
 			}
-			else if (['sidebar-annotation', 'sidebar-selector', 'sidebar-search'].includes(focusManagerRef.current.zone.id)
+			else if ([
+					'sidebar-annotation',
+					'sidebar-selector',
+					'sidebar-search',
+					'view-annotation'
+				].includes(focusManagerRef.current.zone.id)
 				&& isMod && e.key === 'a'
 			) {
-				setSelectedIDs(annotationsViewRef.current.getAnnotations().map(x => x.id));
+				setSelectedIDs(annotationsRef.current.map(x => x.id));
+				e.preventDefault();
 			}
 
 			// Don't bypass keys if focus isn't on an input, contenteditable or button (only for Space),
@@ -932,6 +950,7 @@ const Annotator = React.forwardRef((props, ref) => {
 
 			// Prevent Mod + A, as it selects random things in viewer container and makes them draggable
 			if (isMod && !isShift && e.key === 'a') {
+				setSelectedIDs(annotationsRef.current.map(x => x.id));
 				e.preventDefault();
 			}
 			// Prevent "open file", "download file" PDF.js keyboard shortcuts
@@ -1161,7 +1180,7 @@ const Annotator = React.forwardRef((props, ref) => {
 			return 0;
 		}
 		let selectedIDs = selectedIDsRef.current.slice();
-		let annotations = selectInSidebar ? annotationsViewRef.current.getAnnotations() : annotationsRef.current;
+		let annotations = annotationsRef.current;
 		if (shift && selectedIDs.length) {
 			let annotationIndex = annotations.findIndex(x => x.id === id);
 			let lastSelectedIndex = annotations.findIndex(x => x.id === selectedIDs.slice(-1)[0]);
@@ -1965,12 +1984,7 @@ const Annotator = React.forwardRef((props, ref) => {
 		props.onPopup('openSelectorPopup', data);
 	}, []);
 
-	const handleDeselectAnnotations = useCallback(() => {
-		setSelectedIDs([]);
-		props.onClosePopup();
-	}, []);
-
-	function handleSelectorChange(remove, tag, color) {
+	const handleSelectorChange = useCallback((remove, tag, color) => {
 		let selectedAnnotations = annotationsRef.current.filter(x => draggingAnnotationIDsRef.current.includes(x.id));
 		selectedAnnotations = selectedAnnotations.filter(x => !x.readOnly);
 		let annotations = [];
@@ -1997,7 +2011,12 @@ const Annotator = React.forwardRef((props, ref) => {
 			}
 		}
 		props.onUpdateAnnotations(annotations);
-	}
+	}, []);
+
+	const handleFilterChange = useCallback((filter) => {
+		setFilter(filter);
+		setAnnotations(filterAnnotations(allAnnotationsRef.current, filter));
+	}, []);
 
 	return (
 		<div>
@@ -2008,7 +2027,8 @@ const Annotator = React.forwardRef((props, ref) => {
 				onColorPick={handleToolbarColorClick}
 			/>}
 			<AnnotationsView
-				ref={annotationsViewRef}
+				filter={_filter}
+				allAnnotations={_allAnnotations}
 				annotations={_annotations}
 				selectedIDs={_selectedIDs}
 				expansionState={_expansionState}
@@ -2021,8 +2041,8 @@ const Annotator = React.forwardRef((props, ref) => {
 				onDragStart={handleSidebarAnnotationDragStart}
 				onMenu={handleSidebarAnnotationMenuOpen}
 				onSelectorMenu={handleSelectorMenuOpen}
-				onDeselectAnnotations={handleDeselectAnnotations}
 				onSelectorChange={handleSelectorChange}
+				onChangeFilter={handleFilterChange}
 			/>
 			<Layer
 				selectionColor={_mode === 'highlight' ? _color : selectionColor}

src/components/annotations-view.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 import { FormattedMessage, useIntl } from 'react-intl';
 import cx from 'classnames';
 import { SidebarPreview } from './preview';
-import { searchAnnotations } from '../lib/search';
 import { IconColor, IconUser } from "./icons";
 import { annotationColors } from "../lib/colors";
 
@@ -135,122 +134,50 @@ const Annotation = React.memo((props) => {
 	);
 });
 
-const AnnotationsView = memo(forwardRef(function (props, ref) {
-	const [searchedAnnotations, setSearchedAnnotations] = useState(null);
-	const [query, setQuery] = useState('');
-	const [selectedTags, setSelectedTags] = useState([]);
-	const [selectedColors, setSelectedColors] = useState([]);
-	const [selectedAuthors, setSelectedAuthors] = useState([]);
-	const prevAnnotationsLengthRef = useRef(props.annotations.length);
-
-	useEffect(() => {
-		let annotations = getAnnotations();
-		if (prevAnnotationsLengthRef.current !== annotations.length) {
-			props.onDeselectAnnotations();
-		}
-		prevAnnotationsLengthRef.current = annotations.length;
-	});
-
-	useImperativeHandle(ref, () => ({
-		getAnnotations,
-		clearSelector: () => {
-			setSelectedTags([]);
-			setSelectedColors([]);
-		}
-	}));
-
-	// Deselect tags and colors that no longer exist in any annotation
-	let _selectedColors = [];
-	for (let selectedColor of selectedColors) {
-		if (props.annotations.some(a => a.color === selectedColor)) {
-			_selectedColors.push(selectedColor);
-		}
-	}
-	if (selectedColors.length !== _selectedColors.length) {
-		setSelectedColors(_selectedColors);
-	}
-
-	let _selectedTags = [];
-	for (let selectedTag of selectedTags) {
-		if (props.annotations.some(a => a.tags.some(t => selectedTag === t.name))) {
-			_selectedTags.push(selectedTag);
-		}
-	}
-	if (selectedTags.length !== _selectedTags.length) {
-		setSelectedTags(_selectedTags);
-	}
-
-	function getAnnotations() {
-		let { annotations } = props;
-		if (searchedAnnotations) {
-			let newSearchedAnnotations = [];
-			for (let filteredAnnotation of searchedAnnotations) {
-				let annotation = annotations.find(x => x.id === filteredAnnotation.id);
-				if (annotation) {
-					newSearchedAnnotations.push(annotation);
-				}
-			}
-			annotations = newSearchedAnnotations;
-		}
-		if (selectedTags.length || selectedColors.length || selectedAuthors.length) {
-			annotations = annotations.filter(x => (
-				x.tags.some(t => selectedTags.includes(t.name))
-				|| selectedColors.includes(x.color)
-				|| selectedAuthors.includes(x.authorName)
-			));
-		}
-		return annotations;
-	}
-
+const AnnotationsView = memo(function (props) {
 	function getContainerNode() {
 		return document.getElementById('annotationsView');
 	}
 
-	function search(query) {
-		let { annotations } = props;
-		if (query) {
-			setSearchedAnnotations(searchAnnotations(annotations, query));
-		}
-		else {
-			setSearchedAnnotations(null);
-		}
-	}
-
 	function handleSearchInput(query) {
-		setQuery(query);
-		search(query);
+		props.onChangeFilter({ ...props.filter, query });
 	}
 
 	function handleSearchClear() {
-		setQuery('');
-		search();
+		props.onChangeFilter({ ...props.filter, query: '' });
 	}
 
 	function handleColorClick(color) {
-		if (selectedColors.includes(color)) {
-			setSelectedColors(selectedColors.filter(x => x !== color));
+		let { colors } = props.filter;
+		if (colors.includes(color)) {
+			colors = colors.filter(x => x !== color);
 		}
 		else {
-			setSelectedColors([...selectedColors, color]);
+			colors = [...colors, color];
 		}
+		props.onChangeFilter({ ...props.filter, colors });
 	}
 
-	function handleTagClick(name) {
-		if (selectedTags.includes(name)) {
-			setSelectedTags(selectedTags.filter(x => x !== name));
+	function handleTagClick(tag) {
+		let { tags } = props.filter;
+		if (tags.includes(tag)) {
+			tags = tags.filter(x => x !== tag);
 		}
 		else {
-			setSelectedTags([...selectedTags, name]);
+			tags = [...tags, tag];
 		}
+		props.onChangeFilter({ ...props.filter, tags });
 	}
 
 	function handleAuthorClick(author) {
-		if (selectedAuthors.includes(author)) {
-			setSelectedAuthors(selectedAuthors.filter(x => x !== author));
+		let { authors } = props.filter;
+		if (authors.includes(author)) {
+			authors = authors.filter(x => x !== author);
 		}
 		else {
-			setSelectedAuthors([...selectedAuthors, author]);
+			authors = [...authors, author];
 		}
+		props.onChangeFilter({ ...props.filter, authors });
 	}
 
 	function handleSelectorContextMenu(event) {
@@ -262,7 +189,7 @@ const AnnotationsView = memo(forwardRef(function (props, ref) {
 		props.onSelectorMenu({
 			x: event.screenX,
 			y: event.screenY,
-			enableClearSelection: selectedColors.length || selectedTags.length
+			enableClearSelection: props.filter.colors.length || props.filter.tags.length || props.filter.authors.length
 		});
 	}
 
@@ -271,16 +198,14 @@ const AnnotationsView = memo(forwardRef(function (props, ref) {
 		return null;
 	}
 
-	let annotations = getAnnotations();
-
 	let tags = {};
 	let colors = {};
 	let authors = {};
-	for (let annotation of props.annotations) {
+	for (let annotation of props.allAnnotations) {
 		for (let tag of annotation.tags) {
 			if (!tags[tag.name]) {
 				tags[tag.name] = { ...tag };
-				tags[tag.name].selected = selectedTags.includes(tag.name);
+				tags[tag.name].selected = props.filter.tags.includes(tag.name);
 				tags[tag.name].inactive = true;
 			}
 		}
@@ -289,7 +214,7 @@ const AnnotationsView = memo(forwardRef(function (props, ref) {
 			let predefinedColor = annotationColors.find(x => x[1] === color);
 			colors[color] = {
 				color,
-				selected: selectedColors.includes(color),
+				selected: props.filter.colors.includes(color),
 				inactive: true,
 				name: predefinedColor ? predefinedColor[0] : null
 			};
@@ -298,14 +223,14 @@ const AnnotationsView = memo(forwardRef(function (props, ref) {
 		if (author && !authors[author]) {
 			authors[author] = {
 				author,
-				selected: selectedAuthors.includes(author),
+				selected: props.filter.authors.includes(author),
 				inactive: true,
 				current: author === props.authorName
 			};
 		}
 	}
 
-	for (let annotation of annotations) {
+	for (let annotation of props.annotations) {
 		for (let tag of annotation.tags) {
 			tags[tag.name].inactive = false;
 		}
@@ -362,13 +287,13 @@ const AnnotationsView = memo(forwardRef(function (props, ref) {
 	return ReactDOM.createPortal(
 		<React.Fragment>
 			<AnnotationsViewSearch
-				query={query}
+				query={props.filter.query}
 				onInput={handleSearchInput}
 				onClear={handleSearchClear}
 			/>
 			<div id="annotations" className="annotations">
-				{annotations.length
-					? annotations.map(annotation => (
+				{props.allAnnotations.length
+					? props.annotations.map(annotation => (
 						<Annotation
 							key={annotation.id}
 							isSelected={props.selectedIDs.includes(annotation.id)}
@@ -384,7 +309,7 @@ const AnnotationsView = memo(forwardRef(function (props, ref) {
 							onAnnotationEditorBlur={props.onAnnotationEditorBlur}
 						/>
 					))
-					: !query.length && <div><FormattedMessage id="pdfReader.noAnnotations"/></div>}
+					: !props.filter.query.length && <div><FormattedMessage id="pdfReader.noAnnotations"/></div>}
 			</div>
 			{(!!tags.length || colors.length > 1) && <Selector
 				tags={tags}
@@ -397,6 +322,6 @@ const AnnotationsView = memo(forwardRef(function (props, ref) {
 				onChange={props.onSelectorChange}
 			/>}
 		</React.Fragment>, containerNode);
-}));
+});
 
 export default AnnotationsView;

src/index.zotero.js
@@ -262,7 +262,7 @@ class ViewerInstance {
 				return;
 			}
 			case 'clearSelector': {
-				this._viewer.clearSelector();
+				this._viewer.clearFilter();
 				return;
 			}
 			case 'copy': {

src/lib/search.js
@@ -289,3 +289,63 @@ export function searchAnnotations(annotations, query) {
 
 	return results.map(x => x.annotation);
 }
+
+export function filterAnnotations(annotations, filter) {
+	let { tags, colors, authors } = filter;
+	if (tags.length || colors.length || authors.length) {
+		annotations = annotations.filter(x => (
+			tags && x.tags.some(t => tags.includes(t.name))
+			|| colors && colors.includes(x.color)
+			|| authors && authors.includes(x.authorName)
+		));
+	}
+
+	if (filter.query) {
+		annotations = annotations.slice();
+		let query = filter.query.toLowerCase();
+		let results = [];
+		for (let annotation of annotations) {
+			let errors = null;
+			let match = null;
+
+			if (annotation.text) {
+				match = approximateMatch(annotation.text.toLowerCase(), query, Math.floor(query.length / 5));
+				if (match.length) {
+					errors = Math.min(...match.map(x => x.errors));
+				}
+			}
+
+			if (annotation.comment) {
+				match = approximateMatch(annotation.comment.toLowerCase(), query, Math.floor(query.length / 5));
+				if (match.length) {
+					let er = Math.min(...match.map(x => x.errors));
+					if (errors !== null) {
+						errors = Math.min(errors, er);
+					}
+					else {
+						errors = er;
+					}
+				}
+			}
+
+			if (errors !== null) {
+				results.push({
+					errors,
+					annotation
+				});
+			}
+		}
+
+		annotations = results.sort((a, b) => a.errors - b.errors).map(x => x.annotation);
+	}
+
+	return annotations;
+}
+
+export function cleanFilter(annotations, filter) {
+	let { query, colors, tags, authors } = filter;
+	colors = colors.filter(color => annotations.some(a => a.colors.includes(color)));
+	tags = tags.filter(tag => annotations.some(a => a.tags.some(x => x.name === tag)));
+	authors = authors.filter(author => annotations.some(a => a.authors.includes(author)));
+	return { query, colors, tags, authors };
+}

src/viewer.js
@@ -343,8 +343,8 @@ class Viewer {
 		this.annotatorRef.current.editHighlightedText(data);
 	};
 
-	clearSelector = () => {
-		this.annotatorRef.current.clearSelector();
+	clearFilter = () => {
+		this.annotatorRef.current.clearFilter();
 	};
 
 	navigate = async (location) => {

commit 6f14b5d712395ff19b9e93e9c652b2626f7a80d3

Work around diverging React states when updating annotations list Fixes zotero/zotero#2769 Regression from 4537d755

src/components/annotator.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, { useEffect, useRef, useCallback, useImperativeHandle } from 'react';
-import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 import Layer from './layer';
 import AnnotationsView from './annotations-view';
 import Toolbar from './toolbar';
@@ -526,15 +526,17 @@ const Annotator = React.forwardRef((props, ref) => {
 	useImperativeHandle(ref, () => ({
 		navigate,
 		setAnnotations: (annotations) => {
-			setAllAnnotations(annotations);
-			let filter = cleanFilter(annotations, filterRef.current);
-			setFilter(filter);
-			let filteredAnnotations = filterAnnotations(annotations, filter);
-			setAnnotations(filteredAnnotations);
-			if (filteredAnnotations.length !== annotationsRef.current.length) {
-				setSelectedIDs([]);
-				props.onClosePopup();
-			}
+			ReactDOM.unstable_batchedUpdates(() => {
+				setAllAnnotations(annotations);
+				let filter = cleanFilter(annotations, filterRef.current);
+				setFilter(filter);
+				let filteredAnnotations = filterAnnotations(annotations, filter);
+				setAnnotations(filteredAnnotations);
+				if (filteredAnnotations.length !== annotationsRef.current.length) {
+					setSelectedIDs([]);
+					props.onClosePopup();
+				}
+			});
 		},
 		setColor,
 		setEnableAddToNote,