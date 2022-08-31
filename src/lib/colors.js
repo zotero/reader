65d5acf2c26f86a29895c3d66b3292ff512c3c61
Martynas Bagdonas <git.martynas@gmail.com>
2022-08-31 15:43:36 +0300

    Revert accidental color commit 3027f174

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -5,10 +5,7 @@ export let annotationColors = [
 	['general.red', '#ff6666'],
 	['general.green', '#5fb236'],
 	['general.blue', '#2ea8e5'],
-	['general.purple', '#a28ae5'],
-	['general.orange', '#ec9754'],
-	['general.magenta', '#e56eee'],
-	['general.gray', '#aaaaaa'],
+	['general.purple', '#a28ae5']
 ];
 
 export let selectionColor = navigator.platform.includes('Mac') ? '#71ADFD' : 'Highlight';
