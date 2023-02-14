9410fbc6f09df8fccfeb91af4901828d865f5b0e
Abe Jellinek <jellinek@berkeley.edu>
2023-02-14 11:05:55 -0500

    Use a better example EPUB
    Standard Ebooks' Moby-Dick instead of Project Gutenberg's. More representative
    of the sort of EPUB you'd get from a publisher, I think.

--- demo/epub/annotations.js
+++ demo/epub/annotations.js
@@ -1,96 +1,96 @@
 export default [
 	{
 		type: "highlight",
-		color: "#ff6666",
-		sortIndex: "0000000001|000",
-		pageLabel: "1",
-		position: {
-			type: "FragmentSelector",
-			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
-			value: "epubcfi(/6/6!/4/2/2/2,/1:0,/1:14)"
-		},
-		text: "Brief Contents",
-		comment: "toc",
-		tags: [],
-		id: "5XP6CFG9",
-		dateCreated: "2023-02-13T18:10:02.096Z",
-		dateModified: "2023-02-13T18:10:04.678Z",
-		authorName: "John",
-		isAuthorNameAuthoritative: true
-	},
-	{
-		type: "highlight",
 		color: "#a28ae5",
-		sortIndex: "0000000147|768",
-		pageLabel: "147",
+		sortIndex: "0000000011|000",
+		pageLabel: "11",
 		position: {
 			type: "FragmentSelector",
 			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
-			value: "epubcfi(/6/14!/4/2/4,/4[c001s0002]/1:35,/8[c001s0004]/1:52)"
+			value: "epubcfi(/6/8!/4/2[etymology]/2,/2/1:0,/4/1:58)"
 		},
-		text: "precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth;",
-		comment: "wow!",
+		text: "Etymology\n(Supplied by a Late Consumptive Usher to a Grammar School)",
+		comment: "useful information",
 		tags: [],
-		id: "EVAZ8CW9",
-		dateCreated: "2023-02-13T18:10:12.903Z",
-		dateModified: "2023-02-13T18:10:58.199Z",
+		id: "XTAGKZQJ",
+		dateCreated: "2023-02-13T22:24:06.076Z",
+		dateModified: "2023-02-13T22:24:11.210Z",
 		authorName: "John",
 		isAuthorNameAuthoritative: true
 	},
 	{
 		type: "highlight",
-		color: "#e56eee",
-		sortIndex: "0000000161|754",
-		pageLabel: "161",
+		color: "#5fb236",
+		sortIndex: "0000000151|707",
+		pageLabel: "151",
 		position: {
 			type: "FragmentSelector",
 			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
-			value: "epubcfi(/6/14!/4/2,/10/2[c001p0004]/1:0,/12/2[c001p0005]/1:626)"
+			value: "epubcfi(/6/14!/4/2[chapter-1]/4,/1:0,/1:16)"
 		},
-		text: "But look! here come more crowds, pacing straight for the water, and seemingly bound for a dive. Strange! Nothing will content them but the extremest limit of the land; loitering under the shady lee of yonder warehouses will not suffice. No. They must get just as nigh the water as they possibly can without falling in. And there they stand—miles of them—leagues. Inlanders all, they come from lanes and alleys, streets and avenues—north, east, south, and west. Yet here they all unite. Tell me, does the magnetic virtue of the needles of the compasses of all those ships attract them thither?\n\nOnce more. Say you are in the country; in some high land of lakes. Take almost any path you please, and ten to one it carries you down in a dale, and leaves you there by a pool in the stream. There is magic in it. Let the most absent-minded of men be plunged in his deepest reveries—stand that man on his legs, set his feet a-going, and he will infallibly lead you to water, if water there be in all that region. Should you ever be athirst in the great American desert, try this experiment, if your caravan happen to be supplied with a metaphysical professor. Yes, as every one knows, meditation and water are wedded for ever.",
+		text: "Call me Ishmael.",
 		comment: "",
 		tags: [],
-		id: "KRNM6BZ2",
-		dateCreated: "2023-02-13T18:11:10.903Z",
-		dateModified: "2023-02-13T18:11:10.903Z",
-		authorName: "John",
-		isAuthorNameAuthoritative: true
-	},
-	{
-		type: "highlight",
-		color: "#aaaaaa",
-		sortIndex: "0000000214|769",
-		pageLabel: "214",
-		position: {
-			type: "FragmentSelector",
-			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
-			value: "epubcfi(/6/14!/4/2,/24/2[c001p0011]/1:1111,/28/2[c001p0015]/1:65)"
-		},
-		text: "I take it that this part of the bill must have run something like this:\n\n“GRAND CONTESTED ELECTION FOR THE PRESIDENCY OF THE UNITED STATES.\n\n“WHALING VOYAGE BY ONE ISHMAEL.\n\n“BLOODY BATTLE IN AFFGHANISTAN.”\n\nThough I cannot tell why it was exactly that those stage managers",
-		comment: "",
-		tags: [],
-		id: "GHETIKV8",
-		dateCreated: "2023-02-13T18:10:38.534Z",
-		dateModified: "2023-02-13T18:10:38.534Z",
+		id: "N8BE6M9U",
+		dateCreated: "2023-02-13T22:24:40.535Z",
+		dateModified: "2023-02-13T22:24:40.535Z",
 		authorName: "John",
 		isAuthorNameAuthoritative: true
 	},
 	{
 		type: "highlight",
 		color: "#ffd400",
-		sortIndex: "0000000684|000",
-		pageLabel: "684",
+		sortIndex: "0000002975|824",
+		pageLabel: "2975",
 		position: {
 			type: "FragmentSelector",
 			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
-			value: "epubcfi(/6/30!/4/2,/2/2/1:0,/10/1:278)"
+			value: "epubcfi(/6/102!/4/2[chapter-45],/32/1:0,/34/1:544)"
 		},
-		text: "Chapter 9. The Sermon.\n\nFather Mapple rose, and in a mild voice of unassuming authority ordered the scattered people to condense. “Starboard gangway, there! side away to larboard—larboard gangway to starboard! Midships! midships!”\n\nThere was a low rumbling of heavy sea-boots among the benches, and a still slighter shuffling of women’s shoes, and all was quiet again, and every eye on the preacher.\n\nHe paused a little; then kneeling in the pulpit’s bows, folded his large brown hands across his chest, uplifted his closed eyes, and offered a prayer so deeply devout that he seemed kneeling and praying at the bottom of the sea.\n\nThis ended, in prolonged solemn tones, like the continual tolling of a bell in a ship that is foundering at sea in a fog—in such tones he commenced reading the following hymn; but changing his manner towards the concluding stanzas, burst forth with a pealing exultation and joy—",
+		text: "“By the thirteenth of May our ship was ready to sail, and the next day we were out in the open sea, on our way to Ochotsh. The weather was very clear and fine, but so intolerably cold that we were obliged to keep on our fur clothing. For some days we had very little wind; it was not till the nineteenth that a brisk gale from the northwest sprang up. An uncommon large whale, the body of which was larger than the ship itself, lay almost at the surface of the water, but was not perceived by anyone on board till the moment when the ship, which was in full sail, was almost upon him, so that it was impossible to prevent its striking against him. We were thus placed in the most imminent danger, as this gigantic creature, setting up its back, raised the ship three feet at least out of the water. The masts reeled, and the sails fell altogether, while we who were below all sprang instantly upon the deck, concluding that we had struck upon some rock; instead of this we saw the monster sailing off with the utmost gravity and solemnity. Captain D’Wolf applied immediately to the pumps to examine whether or not the vessel had received any damage from the shock, but we found that very happily it had escaped entirely uninjured.”\n\n",
 		comment: "",
 		tags: [],
-		id: "Y9JSNANR",
-		dateCreated: "2023-02-13T18:10:50.606Z",
-		dateModified: "2023-02-13T18:10:50.606Z",
+		id: "8GK9TZRN",
+		dateCreated: "2023-02-13T22:25:31.812Z",
+		dateModified: "2023-02-13T22:25:34.053Z",
+		authorName: "John",
+		isAuthorNameAuthoritative: true
+	},
+	{
+		type: "highlight",
+		color: "#ff6666",
+		sortIndex: "0000007848|813",
+		pageLabel: "7848",
+		position: {
+			type: "FragmentSelector",
+			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
+			value: "epubcfi(/6/282!/4/2[chapter-135],/28/1:0,/30/1:11)"
+		},
+		text: "In due time the boats were lowered; but as standing in his shallop’s stern, Ahab just hovered upon the point of the descent, he waved to the mate﻿—who held one of the tackle-ropes on deck﻿—and bade him pause.\n\n",
+		comment: "",
+		tags: [],
+		id: "4RL2TQK2",
+		dateCreated: "2023-02-13T22:25:11.428Z",
+		dateModified: "2023-02-13T22:25:15.573Z",
+		authorName: "John",
+		isAuthorNameAuthoritative: true
+	},
+	{
+		type: "highlight",
+		color: "#f19837",
+		sortIndex: "0000007983|690",
+		pageLabel: "7983",
+		position: {
+			type: "FragmentSelector",
+			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
+			value: "epubcfi(/6/286!/4/2[endnotes]/4/2[note-1]/2,/1:0,/1:55)"
+		},
+		text: "See subsequent chapters for something more on this head",
+		comment: "Endnotes with links",
+		tags: [],
+		id: "3B7UZADP",
+		dateCreated: "2023-02-13T22:24:52.726Z",
+		dateModified: "2023-02-13T22:25:02.360Z",
 		authorName: "John",
 		isAuthorNameAuthoritative: true
 	}
