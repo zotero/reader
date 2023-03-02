ff04eb927092f8190613fea65199c2c7738515d7
Abe Jellinek <jellinek@berkeley.edu>
2023-03-02 13:07:49 -0500

    Update EPUB example annotations

--- demo/epub/annotations.js
+++ demo/epub/annotations.js
@@ -9,12 +9,12 @@ export default [
 			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
 			value: "epubcfi(/6/8!/4/2[etymology]/2,/2/1:0,/4/1:58)"
 		},
-		text: "Etymology\n(Supplied by a Late Consumptive Usher to a Grammar School)",
+		text: "Etymology\n\t\t\t\t(Supplied by a Late Consumptive Usher to a Grammar School)",
 		comment: "useful information",
 		tags: [],
 		id: "TX8S2BZM",
 		dateCreated: "2023-02-21T19:35:47.798Z",
-		dateModified: "2023-02-21T19:35:52.865Z",
+		dateModified: "2023-03-02T18:06:26.385Z",
 		authorName: "John",
 		isAuthorNameAuthoritative: true
 	},
@@ -33,7 +33,7 @@ export default [
 		tags: [],
 		id: "PARGMWMZ",
 		dateCreated: "2023-02-21T19:36:03.145Z",
-		dateModified: "2023-02-21T19:36:03.145Z",
+		dateModified: "2023-03-02T18:06:30.288Z",
 		authorName: "John",
 		isAuthorNameAuthoritative: true
 	},
@@ -41,18 +41,18 @@ export default [
 		type: "highlight",
 		color: "#ffd400",
 		sortIndex: "00050|00011246",
-		pageLabel: "272",
+		pageLabel: "236",
 		position: {
 			type: "FragmentSelector",
 			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
 			value: "epubcfi(/6/102!/4/2[chapter-45],/32/1:0,/34/1:544)"
 		},
-		text: "“By the thirteenth of May our ship was ready to sail, and the next day we were out in the open sea, on our way to Ochotsh. The weather was very clear and fine, but so intolerably cold that we were obliged to keep on our fur clothing. For some days we had very little wind; it was not till the nineteenth that a brisk gale from the northwest sprang up. An uncommon large whale, the body of which was larger than the ship itself, lay almost at the surface of the water, but was not perceived by anyone on board till the moment when the ship, which was in full sail, was almost upon him, so that it was impossible to prevent its striking against him. We were thus placed in the most imminent danger, as this gigantic creature, setting up its back, raised the ship three feet at least out of the water. The masts reeled, and the sails fell altogether, while we who were below all sprang instantly upon the deck, concluding that we had struck upon some rock; instead of this we saw the monster sailing off with the utmost gravity and solemnity. Captain D’Wolf applied immediately to the pumps to examine whether or not the vessel had received any damage from the shock, but we found that very happily it had escaped entirely uninjured.”\n\nNow, the Captain D’Wolf here alluded to as commanding the ship in question, is a New Englander, who, after a long life of unusual adventures as a sea-captain, this day resides in the village of Dorchester near Boston. I have the honor of being a nephew of his. I have particularly questioned him concerning this passage in Langsdorff. He substantiates every word. The ship, however, was by no means a large one: a Russian craft built on the Siberian coast, and purchased by my uncle after bartering away the vessel in which he sailed from home.",
+		text: "“By the thirteenth of May our ship was ready to sail, and the next day we were out in the open sea, on our way to Ochotsh. The weather was very clear and fine, but so intolerably cold that we were obliged to keep on our fur clothing. For some days we had very little wind; it was not till the nineteenth that a brisk gale from the northwest sprang up. An uncommon large whale, the body of which was larger than the ship itself, lay almost at the surface of the water, but was not perceived by anyone on board till the moment when the ship, which was in full sail, was almost upon him, so that it was impossible to prevent its striking against him. We were thus placed in the most imminent danger, as this gigantic creature, setting up its back, raised the ship three feet at least out of the water. The masts reeled, and the sails fell altogether, while we who were below all sprang instantly upon the deck, concluding that we had struck upon some rock; instead of this we saw the monster sailing off with the utmost gravity and solemnity. Captain D’Wolf applied immediately to the pumps to examine whether or not the vessel had received any damage from the shock, but we found that very happily it had escaped entirely uninjured.”\n\t\t\tNow, the Captain D’Wolf here alluded to as commanding the ship in question, is a New Englander, who, after a long life of unusual adventures as a sea-captain, this day resides in the village of Dorchester near Boston. I have the honor of being a nephew of his. I have particularly questioned him concerning this passage in Langsdorff. He substantiates every word. The ship, however, was by no means a large one: a Russian craft built on the Siberian coast, and purchased by my uncle after bartering away the vessel in which he sailed from home.",
 		comment: "",
 		tags: [],
 		id: "4JKDWLSF",
 		dateCreated: "2023-02-21T19:36:34.563Z",
-		dateModified: "2023-02-21T19:36:34.563Z",
+		dateModified: "2023-03-02T18:06:34.994Z",
 		authorName: "John",
 		isAuthorNameAuthoritative: true
 	},
@@ -60,18 +60,18 @@ export default [
 		type: "highlight",
 		color: "#ff6666",
 		sortIndex: "00140|00006394",
-		pageLabel: "722",
+		pageLabel: "638",
 		position: {
 			type: "FragmentSelector",
 			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
 			value: "epubcfi(/6/282!/4/2[chapter-135],/28/1:0,/30/1:11)"
 		},
-		text: "In due time the boats were lowered; but as standing in his shallop’s stern, Ahab just hovered upon the point of the descent, he waved to the mate﻿—who held one of the tackle-ropes on deck﻿—and bade him pause.\n\n“Starbuck!”",
+		text: "In due time the boats were lowered; but as standing in his shallop’s stern, Ahab just hovered upon the point of the descent, he waved to the mate﻿—who held one of the tackle-ropes on deck﻿—and bade him pause.\n\t\t\t“Starbuck!”",
 		comment: "",
 		tags: [],
 		id: "YMJAWZXH",
 		dateCreated: "2023-02-21T19:36:16.574Z",
-		dateModified: "2023-02-21T19:36:16.574Z",
+		dateModified: "2023-03-02T18:06:40.385Z",
 		authorName: "John",
 		isAuthorNameAuthoritative: true
 	},
@@ -79,7 +79,7 @@ export default [
 		type: "highlight",
 		color: "#f19837",
 		sortIndex: "00142|00000045",
-		pageLabel: "734",
+		pageLabel: "648",
 		position: {
 			type: "FragmentSelector",
 			conformsTo: "http://www.idpf.org/epub/linking/cfi/epub-cfi.html",
@@ -90,7 +90,7 @@ export default [
 		tags: [],
 		id: "YUTUT8H3",
 		dateCreated: "2023-02-21T19:36:44.833Z",
-		dateModified: "2023-02-21T19:36:49.643Z",
+		dateModified: "2023-03-02T18:06:44.558Z",
 		authorName: "John",
 		isAuthorNameAuthoritative: true
 	}
