bfd15731de2f5a0d1fc4eb9a439157ffdb267c4c
Martynas Bagdonas <git.martynas@gmail.com>
2023-04-13 12:57:17 +0300

    Fix navigation regression when selecting an annotation in sidebar

--- src/common/components/sidebar/annotations-view.js
+++ src/common/components/sidebar/annotations-view.js
@@ -309,7 +309,7 @@ const AnnotationsView = memo(React.forwardRef((props, ref) => {
 				setExpansionState(2);
 			}
 			if (!(props.selectedIDs.length === 1 && props.selectedIDs[0] === id)) {
-				props.onSelectAnnotations([id], event);
+				props.onSelectAnnotations([id], false, event);
 			}
 		}
 	}
