4ed256047fd4e5391ca019e780ac3e7778e21d44
Abe Jellinek <jellinek@berkeley.edu>
2023-06-27 15:42:24 -0400

    Produce nicer unique selectors using adjacent-sibling combinator
    And check - to be absolutely sure - that the selector actually matches the
    element we want it to match.

--- src/dom/common/lib/unique-selector.ts
+++ src/dom/common/lib/unique-selector.ts
@@ -1,3 +1,5 @@
+import { closestElement } from "./nodes";
+
 /**
  * Generate a CSS selector uniquely pointing to node's closest Element ancestor, relative to root.
  */
@@ -7,51 +9,63 @@ export function getUniqueSelectorContaining(node: Node, root: Element): string |
 		return null;
 	}
 	// Get the closest element to the node (which may be the node itself)
-	let element = null;
-	for (let n: Node | null = node; n !== null; n = n.parentNode) {
-		if (n.nodeType == Node.ELEMENT_NODE) {
-			element = n as Element;
-			break;
-		}
-	}
+	let element = closestElement(node);
 	if (!element) {
 		return null;
 	}
+	let originalElement = element;
+
+	let testSelector = (selector: string) => {
+		return root.querySelectorAll(selector).length == 1 && root.querySelector(selector) == originalElement;
+	};
+
 	let selector = '';
 	while (element && element !== root) {
 		let joiner = selector ? ' > ' : '';
 		if (element.id) {
-			return `#${element.id.replace(/([^a-zA-Z0-9\u00A0-\uFFFF-_])/g, '\\$1')}` + joiner + selector;
+			return `#${CSS.escape(element.id)}` + joiner + selector;
 		}
+
 		let tagName = element.tagName.toLowerCase();
-		let childSelector;
+
+		let prevSibling = element.previousElementSibling;
+		if (prevSibling && prevSibling.id) {
+			let prevSiblingIDSelector = `#${CSS.escape(prevSibling.id)} + ${tagName}${joiner}${selector}`;
+			if (testSelector(prevSiblingIDSelector)) {
+				return prevSiblingIDSelector;
+			}
+		}
+
+		let childPseudoclass;
 		if (element.matches(':only-of-type') || element.matches(':only-child')) {
-			childSelector = '';
+			childPseudoclass = '';
 		}
 		else if (element.matches(':first-child')) {
-			childSelector = ':first-child';
+			childPseudoclass = ':first-child';
 		}
 		else if (element.matches(':first-of-type')) {
-			childSelector = ':first-of-type';
+			childPseudoclass = ':first-of-type';
 		}
 		else if (element.matches(':last-child')) {
-			childSelector = ':last-child';
+			childPseudoclass = ':last-child';
 		}
 		else if (element.matches(':last-of-type')) {
-			childSelector = ':last-of-type';
+			childPseudoclass = ':last-of-type';
 		}
 		else if (element.parentElement) {
-			childSelector = `:nth-child(${[...element.parentElement.children].indexOf(element) + 1})`;
+			childPseudoclass = `:nth-child(${[...element.parentElement.children].indexOf(element) + 1})`;
 		}
 		else {
 			break;
 		}
-		selector = tagName + childSelector + joiner + selector;
-		element = element.parentElement;
 
-		if (root.querySelectorAll(selector).length == 1) {
+		selector = tagName + childPseudoclass + joiner + selector;
+
+		if (testSelector(selector)) {
 			return selector;
 		}
+
+		element = element.parentElement;
 	}
 	return null;
 }
