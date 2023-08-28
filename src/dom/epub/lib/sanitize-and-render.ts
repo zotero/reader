0335240fed98a35016e86c9cb1087c335897eab0
Abe Jellinek <jellinek@berkeley.edu>
2023-08-28 17:50:10 -0400

    Support vertical text with EPUB-specific CSS

--- src/dom/epub/lib/sanitize-and-render.ts
+++ src/dom/epub/lib/sanitize-and-render.ts
@@ -94,16 +94,18 @@ export class StyleScoper {
 		let scopeClass = `__scope_${this._sheets.size}`;
 		this._sheets.set(css, { scopeClass });
 
+		let cssModified = this._rewriteEPUBProperties(css);
+
 		let sheet;
 		let added = false;
 		try {
 			sheet = new this._document.defaultView!.CSSStyleSheet();
-			await sheet.replace(css);
+			await sheet.replace(cssModified);
 		}
 		catch (e) {
 			// Constructor not available
 			let style = this._document.createElement('style');
-			style.innerHTML = css;
+			style.innerHTML = cssModified;
 			if (style.sheet) {
 				sheet = style.sheet;
 			}
@@ -198,7 +200,55 @@ export class StyleScoper {
 			}
 		}
 	}
+
+	private _rewriteEPUBProperties(css: string): string {
+		for (let [re, replacement] of EPUB_CSS_REPLACEMENTS) {
+			css = css.replace(re, replacement);
+		}
+		return css;
+	}
 }
+
+// Mappings based on https://github.com/JayPanoz/postcss-epub-interceptor/blob/1aca86e1f4f996f9ec3a8735bd70e673f3e0f504/index.js
+const EPUB_CSS_REPLACEMENTS: [RegExp, string][] = Object.entries({
+	'text-transform': ['text-transform', {
+		'-epub-fullwidth': 'full-width'
+	}],
+	'-epub-hyphens': ['hyphens', {}],
+	'-epub-line-break': ['line-break', {}],
+	'-epub-text-align-last': ['text-align-last', {}],
+	'-epub-word-break': ['word-break', {}],
+	'-epub-text-emphasis': ['text-emphasis', {}],
+	'-epub-text-emphasis-color': ['text-emphasis-color', {}],
+	'-epub-text-emphasis-style': ['text-emphasis-style', {}],
+	'-epub-text-emphasis-position': ['text-emphasis-position', {}],
+	'-epub-text-underline-position': ['text-underline-position', {
+		alphabetic: 'auto'
+	}],
+	'-epub-ruby-position': ['ruby-position', {}],
+	'-epub-writing-mode': ['writing-mode', {}],
+	'-epub-text-orientation': ['text-orientation', {
+		'vertical-right': 'mixed',
+		'sideways-right': 'sideways',
+		'rotate-right': 'sideways',
+		'rotate-normal': 'sideways'
+	}],
+	'-epub-text-combine': ['text-combine-upright', {
+		horizontal: 'all'
+	}],
+	'-epub-text-combine-horizontal': ['text-combine-upright', {}],
+	'-epub-text-combine-upright': ['text-combine-upright', {}],
+}).flatMap(([rule, [newRule, valueMapping]]) => {
+	// That's right: we're parsing CSS with regular expressions
+	// This is unlikely to cause false positives
+	let ruleRe = new RegExp(`(^|[^a-zA-Z-])${rule}(\\s*:)`, 'gi');
+	let ruleReplacement = '$1' + newRule + '$2';
+	let valueReplacements: [RegExp, string][] = Object.entries(valueMapping).map(([value, newValue]) => {
+		let valueRe = new RegExp(`((?:^|[^a-zA-Z-])${newRule}\\s*:\\s*)${value}($|[^a-zA-Z-])`, 'gi');
+		return [valueRe, '$1' + newValue + '$2'];
+	});
+	return [[ruleRe, ruleReplacement], ...valueReplacements];
+});
 
 type SheetMetadata = {
 	sheet?: CSSStyleSheet;
