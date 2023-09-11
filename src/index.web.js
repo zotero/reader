abf4f6225580129e589c970f38e32465128988bf
Tom Najdek <tom@doppnet.com>
2023-09-11 19:48:36 +0200

    Fix epubs not loading in web-library

--- src/index.web.js
+++ src/index.web.js
@@ -5,6 +5,12 @@ window.createReader = (options) => {
 		throw new Error('Reader is already initialized');
 	}
 	options.platform = 'web';
+	if (window.frameElement) {
+		// This fix is needed for epubs to work in web-library. Otherwise instanceof
+		// inside epubjs code check fails (window.Uint8Array != window.top.Uint8Array)
+		// and epubs are not loaded.
+		window.ArrayBuffer = window.top.ArrayBuffer;
+	}
 	let reader = new Reader({
 		onOpenContextMenu: (params) => {
 			reader.openContextMenu(params);
