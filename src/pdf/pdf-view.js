a4af150a8b6d4a51722a494df9cf5381b17c19a9
Bogdan Abaev <bogdan@zotero.org>
2024-09-09 22:38:38 -0700

    don't alter aria-label of the page
    Have pdfjs handle it

--- src/pdf/pdf-view.js
+++ src/pdf/pdf-view.js
@@ -528,8 +528,6 @@ class PDFView {
 		for (let page of this._pages) {
 			if (!pageIndexes || pageIndexes.includes(page.pageIndex)) {
 				page.render();
-				// Aria label with page index set by pdf.js is off by one, so fix it here
-				page.originalPage.div.dataset.l10nArgs = JSON.stringify({ page: this._getPageLabel(page.pageIndex) });
 			}
 		}
 	}
