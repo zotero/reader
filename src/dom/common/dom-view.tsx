f57a3a32ebf109cb7e970a2bfc5d1dc959a7caa6
Abe Jellinek <jellinek@berkeley.edu>
2024-11-15 10:31:09 -0500

    Fix dummy selection remaining in some cases

--- src/dom/common/dom-view.tsx
+++ src/dom/common/dom-view.tsx
@@ -966,6 +966,7 @@ abstract class DOMView<State extends DOMViewState, Data> {
 				else {
 					newRange.setStart(oldRange.startContainer, oldRange.startOffset);
 				}
+				selection.removeAllRanges();
 
 				if (newRange.collapsed) {
 					return;
@@ -979,7 +980,6 @@ abstract class DOMView<State extends DOMViewState, Data> {
 					console.warn('Invalid selector after resize');
 				}
 				this._options.onUpdateAnnotations([annotation]);
-				selection.removeAllRanges();
 			}
 
 			event.preventDefault();
