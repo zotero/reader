zotero/reader

commit 01c276041caf781aecfe6f1c978ffde516eaf80d

Snapshot: Add focus mode (#161)

src/common/annotation-manager.js
@@ -13,7 +13,8 @@ class AnnotationManager {
 			query: '',
 			colors: [],
 			tags: [],
-			authors: []
+			authors: [],
+			hiddenIDs: [],
 		};
 		this._readOnly = options.readOnly;
 		this._authorName = options.authorName;
@@ -398,7 +399,12 @@ class AnnotationManager {
 		this._annotations.forEach(x => delete x._score);
 
 		let annotations = this._annotations.slice();
-		let { tags, colors, authors } = this._filter;
+		let { tags, colors, authors, query, hiddenIDs } = this._filter;
+
+		if (hiddenIDs.length) {
+			annotations = annotations.filter(x => !hiddenIDs.includes(x.id));
+		}
+
 		if (tags.length || colors.length || authors.length) {
 			annotations = annotations.filter(x => (
 				tags && x.tags.some(t => tags.includes(t.name))
@@ -407,9 +413,9 @@ class AnnotationManager {
 			));
 		}
 
-		if (this._filter.query) {
+		if (query) {
 			annotations = annotations.slice();
-			let query = this._filter.query.toLowerCase();
+			let query = query.toLowerCase();
 			let results = [];
 			for (let annotation of annotations) {
 				let errors = null;

src/common/components/modal-popup/appearance-popup.js
@@ -3,7 +3,7 @@ import cx from 'classnames';
 
 import IconRevert from '../../../../res/icons/16/revert.svg';
 import { FormattedMessage, useIntl } from 'react-intl';
-import { DEFAULT_EPUB_APPEARANCE } from '../../../dom/epub/defines';
+import { DEFAULT_REFLOWABLE_APPEARANCE } from '../../../dom/common/defines';
 
 import IconColumnDouble from '../../../../res/icons/16/column-double.svg';
 import IconColumnSingle from '../../../../res/icons/16/column-single.svg';
@@ -18,14 +18,17 @@ import IconSplitVertical from '../../../../res/icons/16/split-vertical.svg';
 import IconSpreadEven from '../../../../res/icons/16/spread-even.svg';
 import IconSpreadNone from '../../../../res/icons/16/spread-none.svg';
 import IconSpreadOdd from '../../../../res/icons/16/spread-odd.svg';
+import IconX from '../../../../res/icons/16/x-8.svg';
 import IconOptions from '../../../../res/icons/16/options.svg';
 import IconPlus from '../../../../res/icons/20/plus.svg';
 import { getCurrentColorScheme, getPopupCoordinatesFromClickEvent } from '../../lib/utilities';
 import { ReaderContext } from '../../reader';
 import { DEFAULT_THEMES } from '../../defines';
 
-function EPUBAppearance({ params, enablePageWidth, onChange }) {
+function ReflowableAppearanceSection({ params, enablePageWidth, onChange }) {
 	const intl = useIntl();
+
+	const { type } = useContext(ReaderContext);
 
 	if (!params) {
 		// Not initialized yet - wait
@@ -43,12 +46,12 @@ function EPUBAppearance({ params, enablePageWidth, onChange }) {
 	}
 
 	function handleRevert(name) {
-		params[name] = DEFAULT_EPUB_APPEARANCE[name];
+		params[name] = DEFAULT_REFLOWABLE_APPEARANCE[name];
 		onChange(params);
 	}
 
 	return (
-		<div className="epub-appearance">
+		<div className="reflowable-appearance">
 			<div className="row">
 				<label htmlFor="line-height"><FormattedMessage id="pdfReader.epubAppearance.lineHeight"/></label>
 				<input
@@ -67,7 +70,7 @@ function EPUBAppearance({ params, enablePageWidth, onChange }) {
 				<button
 					data-tabstop={1}
 					tabIndex={-1}
-					className={cx('toolbar-button', { hidden: params.lineHeight === DEFAULT_EPUB_APPEARANCE.lineHeight })}
+					className={cx('toolbar-button', { hidden: params.lineHeight === DEFAULT_REFLOWABLE_APPEARANCE.lineHeight })}
 					aria-label={intl.formatMessage({ id: 'pdfReader.epubAppearance.lineHeight.revert' })}
 					onClick={() => handleRevert('lineHeight')}
 				><IconRevert/></button>
@@ -91,7 +94,7 @@ function EPUBAppearance({ params, enablePageWidth, onChange }) {
 				<button
 					data-tabstop={1}
 					tabIndex={-1}
-					className={cx('toolbar-button', { hidden: params.wordSpacing === DEFAULT_EPUB_APPEARANCE.wordSpacing })}
+					className={cx('toolbar-button', { hidden: params.wordSpacing === DEFAULT_REFLOWABLE_APPEARANCE.wordSpacing })}
 					aria-label={intl.formatMessage({ id: 'pdfReader.epubAppearance.wordSpacing.revert' })}
 					onClick={() => handleRevert('wordSpacing')}
 				><IconRevert/></button>
@@ -115,7 +118,7 @@ function EPUBAppearance({ params, enablePageWidth, onChange }) {
 				<button
 					data-tabstop={1}
 					tabIndex={-1}
-					className={cx('toolbar-button', { hidden: params.letterSpacing === DEFAULT_EPUB_APPEARANCE.letterSpacing })}
+					className={cx('toolbar-button', { hidden: params.letterSpacing === DEFAULT_REFLOWABLE_APPEARANCE.letterSpacing })}
 					aria-label={intl.formatMessage({ id: 'pdfReader.epubAppearance.letterSpacing.revert' })}
 					onClick={() => handleRevert('letterSpacing')}
 				><IconRevert/></button>
@@ -140,27 +143,29 @@ function EPUBAppearance({ params, enablePageWidth, onChange }) {
 				<button
 					data-tabstop={1}
 					tabIndex={-1}
-					className={cx('toolbar-button', { hidden: params.pageWidth === DEFAULT_EPUB_APPEARANCE.pageWidth })}
+					className={cx('toolbar-button', { hidden: params.pageWidth === DEFAULT_REFLOWABLE_APPEARANCE.pageWidth })}
 					aria-label={intl.formatMessage({ id: 'pdfReader.epubAppearance.pageWidth.revert' })}
 					onClick={() => handleRevert('pageWidth')}
 					disabled={!enablePageWidth}
 				><IconRevert/></button>
 			</div>
 
-			<div className="option">
+			{type === 'epub' && (
+				<div className="option">
 					<label htmlFor="use-original-font"><FormattedMessage
 						id="pdfReader.epubAppearance.useOriginalFont"/></label>
 					<input
 						data-tabstop={1}
-						tabIndex={-1}
-						className="switch"
+						tabIndex={-1}className="switch"
 						type="checkbox"
 						id="use-original-font"
 						name="useOriginalFont"
 						checked={params.useOriginalFont}
 						onChange={handleChange}
 					/>
-			</div>
+
+				</div>
+			)}
 		</div>
 	);
 }
@@ -363,11 +368,28 @@ function AppearancePopup(props) {
 						</div>
 					</div>
 				</div>
-				{type === 'epub' && (
+				{type === 'snapshot' && (
 					<div className="group">
-						<EPUBAppearance
+						<div className="option">
+							<label htmlFor="focus-mode-enabled"><FormattedMessage id="pdfReader.focusMode"/></label>
+							<input
+								data-tabstop={1}
+								tabIndex={-1}
+								className="switch"
+								type="checkbox"
+								id="focus-mode-enabled"
+								checked={props.viewStats.focusModeEnabled}
+								onChange={e => props.onChangeFocusModeEnabled(e.target.checked)}
+							/>
+						</div>
+					</div>
+				)}
+				{(type === 'epub' || type === 'snapshot' && props.viewStats.focusModeEnabled) && (
+					<div className="group">
+						<ReflowableAppearanceSection
 							params={props.viewStats.appearance}
-							enablePageWidth={props.viewStats.flowMode !== 'paginated' || props.viewStats.spreadMode === 0}
+							enablePageWidth={type === 'snapshot'
+								|| props.viewStats.flowMode !== 'paginated' || props.viewStats.spreadMode === 0}
 							onChange={props.onChangeAppearance}
 						/>
 					</div>

src/common/components/reader-ui.js
@@ -233,6 +233,7 @@ const ReaderUI = React.forwardRef((props, ref) => {
 					onChangeSpreadMode={props.onChangeSpreadMode}
 					onChangeFlowMode={props.onChangeFlowMode}
 					onChangeAppearance={props.onChangeAppearance}
+					onChangeFocusModeEnabled={props.onChangeFocusModeEnabled}
 					onAddTheme={props.onAddTheme}
 					onChangeTheme={props.onChangeTheme}
 					onOpenThemeContextMenu={props.onOpenThemeContextMenu}

src/common/reader.js
@@ -369,6 +369,7 @@ class Reader {
 							onSetDataTransferAnnotations={this._handleSetDataTransferAnnotations.bind(this)}
 							onOpenLink={this._onOpenLink}
 							onChangeAppearance={this._handleAppearanceChange.bind(this)}
+							onChangeFocusModeEnabled={this._handleFocusModeEnabledChange.bind(this)}
 							onChangeFindState={this._handleFindStateChange.bind(this)}
 							onFindNext={this.findNext.bind(this)}
 							onFindPrevious={this.findPrevious.bind(this)}
@@ -536,7 +537,7 @@ class Reader {
 			this._secondaryView?.setFindState(this._state.secondaryViewFindState);
 		}
 
-		if (this._type === 'epub') {
+		if (this._type === 'epub' || this._type === 'snapshot') {
 			if (this._state.fontFamily !== previousState.fontFamily) {
 				this._primaryView?.setFontFamily(this._state.fontFamily);
 				this._secondaryView?.setFontFamily(this._state.fontFamily);
@@ -752,9 +753,24 @@ class Reader {
 	}
 
 	_handleAppearanceChange(params) {
-		this._ensureType('epub');
+		this._ensureType('epub', 'snapshot');
 		this._primaryView?.setAppearance(params);
 		this._secondaryView?.setAppearance(params);
+	}
+
+	_handleFocusModeEnabledChange(enabled) {
+		this._ensureType('snapshot');
+		try {
+			this._primaryView?.setFocusModeEnabled(enabled);
+			this._secondaryView?.setFocusModeEnabled(enabled);
+		}
+		catch (e) {
+			console.error(e);
+			this.setErrorMessage(this._getString('pdfReader.focusMode.notSupported'));
+			setTimeout(() => {
+				this.setErrorMessage(null);
+			}, 5000);
+		}
 	}
 
 	_handleFindStateChange(primary, params) {
@@ -993,7 +1009,11 @@ class Reader {
 			let annotationType = this._getString(`pdfReader.${annotation.type}Annotation`);
 			let annotationContent = `${annotationType}. ${annotation.text || annotation.comment}`;
 			this.setA11yMessage(annotationContent);
-		}
+		};
+
+		let onSetHiddenAnnotations = (ids) => {
+			this._annotationManager.setFilter({ hiddenIDs: ids });
+		};
 
 		let getLocalizedString = (name) => this._getString(name);
 
@@ -1047,6 +1067,7 @@ class Reader {
 			onKeyDown,
 			onKeyUp,
 			onFocusAnnotation,
+			onSetHiddenAnnotations,
 			getLocalizedString
 		};
 

src/common/stylesheets/components/_modal-popup.scss
@@ -389,7 +389,7 @@
 		}
 	}
 
-	.epub-appearance {
+	.reflowable-appearance {
 		display: flex;
 		flex-direction: column;
 		gap: 12px;

src/common/types.ts
@@ -1,5 +1,5 @@
 import { Selector } from "../dom/common/lib/selector";
-import { EPUBAppearance } from "../dom/epub/epub-view";
+import { ReflowableAppearance } from "../dom/common/dom-view";
 
 export type ToolType =
 	| 'highlight'
@@ -110,9 +110,10 @@ export type ViewStats = {
 	scrollMode?: number;
 	spreadMode?: number;
 	flowMode?: string;
-	appearance?: Partial<EPUBAppearance>;
+	appearance?: Partial<ReflowableAppearance>;
 	fontFamily?: string;
 	outlinePath?: number[];
+	focusModeEnabled?: boolean;
 };
 
 export type AnnotationPopupParams<A extends Annotation = Annotation> = {
@@ -146,7 +147,11 @@ type ImagePopupParams = {
 	rect: ArrayRect;
 }
 
-export type OverlayPopupParams = FootnotePopupParams | LinkPopupParams | ImagePopupParams
+type HiddenInFocusModeParams = {
+	type: 'hiddenInFocusMode';
+}
+
+export type OverlayPopupParams = FootnotePopupParams | LinkPopupParams | ImagePopupParams | HiddenInFocusModeParams;
 
 export type ArrayRect = [left: number, top: number, right: number, bottom: number];
 

src/dom/common/defines.ts
@@ -1,0 +1,9 @@
+import { PageWidth, ReflowableAppearance } from "./dom-view";
+
+export const DEFAULT_REFLOWABLE_APPEARANCE: ReflowableAppearance = Object.freeze({
+	lineHeight: 1.2,
+	wordSpacing: 0,
+	letterSpacing: 0,
+	pageWidth: PageWidth.Normal,
+	useOriginalFont: false,
+});

src/dom/common/dom-view.tsx
@@ -63,6 +63,7 @@ import {
 } from "./lib/rect";
 import { History } from "../../common/lib/history";
 import { closestMathTeX } from "./lib/math";
+import { DEFAULT_REFLOWABLE_APPEARANCE } from "./defines";
 
 abstract class DOMView<State extends DOMViewState, Data> {
 	readonly MIN_SCALE = 0.6;
@@ -162,6 +163,8 @@ abstract class DOMView<State extends DOMViewState, Data> {
 	protected _a11yVirtualCursorTarget: Node | null;
 
 	protected _a11yShouldFocusVirtualCursorTarget: boolean;
+
+	appearance?: ReflowableAppearance;
 
 	scale = 1;
 
@@ -315,7 +318,16 @@ abstract class DOMView<State extends DOMViewState, Data> {
 		});
 	}
 
-	protected abstract _handleViewCreated(viewState: Partial<Readonly<State>>): MaybePromise<void>;
+	protected async _handleViewCreated(viewState: Partial<Readonly<State>>): Promise<void> {
+		this.setHyphenate(this._options.hyphenate ?? true);
+
+		if (viewState.appearance) {
+			this.setAppearance(viewState.appearance);
+		}
+		else {
+			this.setAppearance(DEFAULT_REFLOWABLE_APPEARANCE);
+		}
+	}
 
 	// ***
 	// Utilities for annotations - abstractions over the specific types of selectors used by the two views
@@ -1750,6 +1762,43 @@ abstract class DOMView<State extends DOMViewState, Data> {
 		this._outline = outline;
 	}
 
+	setAppearance(partialAppearance: Partial<ReflowableAppearance>) {
+		let appearance = {
+			...DEFAULT_REFLOWABLE_APPEARANCE,
+			...partialAppearance
+		};
+		this.appearance = appearance;
+		this._iframeDocument.documentElement.style.setProperty('--content-line-height-adjust', String(appearance.lineHeight));
+		this._iframeDocument.documentElement.style.setProperty('--content-word-spacing-adjust', String(appearance.wordSpacing));
+		this._iframeDocument.documentElement.style.setProperty('--content-letter-spacing-adjust', String(appearance.letterSpacing));
+		this._iframeDocument.documentElement.classList.toggle('use-original-font', appearance.useOriginalFont);
+
+		let pageWidth;
+		switch (appearance.pageWidth) {
+			case PageWidth.Narrow:
+				pageWidth = 'narrow';
+				break;
+			case PageWidth.Normal:
+				pageWidth = 'normal';
+				break;
+			case PageWidth.Full:
+				pageWidth = 'full';
+				break;
+		}
+		this._iframeDocument.documentElement.dataset.pageWidth = pageWidth;
+		this._handleViewUpdate();
+	}
+
+	setFontFamily(fontFamily: string) {
+		this._iframeDocument.documentElement.style.setProperty('--content-font-family', fontFamily);
+		this._renderAnnotations(true);
+	}
+
+	setHyphenate(hyphenate: boolean) {
+		this._iframeDocument.documentElement.classList.toggle('hyphenate', hyphenate);
+		this._renderAnnotations(true);
+	}
+
 	// ***
 	// Public methods to control the view from the outside
 	// ***
@@ -1871,6 +1920,7 @@ export type DOMViewOptions<State extends DOMViewState, Data> = {
 	onKeyDown: (event: KeyboardEvent) => void;
 	onEPUBEncrypted: () => void;
 	onFocusAnnotation: (annotation: WADMAnnotation) => void;
+	onSetHiddenAnnotations: (ids: string[]) => void;
 	getLocalizedString: (name: string) => string;
 	data: Data & {
 		buf?: Uint8Array,
@@ -1880,6 +1930,7 @@ export type DOMViewOptions<State extends DOMViewState, Data> = {
 
 export interface DOMViewState {
 	scale?: number;
+	appearance?: Partial<ReflowableAppearance>;
 }
 
 export interface CustomScrollIntoViewOptions extends Omit<ScrollIntoViewOptions, 'inline'> {
@@ -1890,6 +1941,20 @@ export interface CustomScrollIntoViewOptions extends Omit<ScrollIntoViewOptions,
 
 export interface NavigateOptions extends CustomScrollIntoViewOptions {
 	skipHistory?: boolean;
+}
+
+export interface ReflowableAppearance {
+	lineHeight: number;
+	wordSpacing: number;
+	letterSpacing: number;
+	pageWidth: PageWidth;
+	useOriginalFont: boolean;
+}
+
+export const enum PageWidth {
+	Narrow = -1,
+	Normal = 0,
+	Full = 1
 }
 
 export default DOMView;

commit c39eb13fa275d6dc14253a8c55e85ae66221d45a

Fix annotation search
https://forums.zotero.org/discussion/125078/7-1-beta-41-annotation-search-in-the-reader-does-not-work-anymore

Regression in 01c276041caf781aecfe6f1c978ffde516eaf80d

src/common/annotation-manager.js
@@ -415,7 +415,7 @@ class AnnotationManager {
 
 		if (query) {
 			annotations = annotations.slice();
-			let query = query.toLowerCase();
+			query = query.toLowerCase();
 			let results = [];
 			for (let annotation of annotations) {
 				let errors = null;