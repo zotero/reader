zotero/reader

commit 23575f6ed08908f84b0e4d64b3b9ef85dffe7c9e

EPUB/Snapshot: Add onBackdropTap
And in the process, rework how we handle pointer events. The current
flow now adds its listeners to the body, so it can intercept them before
the view sees them.

src/dom/common/dom-view.tsx
@@ -262,12 +262,12 @@ abstract class DOMView<State extends DOMViewState, Data> {
 		this._iframeWindow.addEventListener('keydown', this._handleKeyDown.bind(this), true);
 		this._iframeWindow.addEventListener('keyup', this._handleKeyUp.bind(this));
 		this._iframeWindow.addEventListener('click', this._handleClick.bind(this));
-		this._iframeDocument.body.addEventListener('pointerover', this._handlePointerOver.bind(this));
-		this._iframeDocument.body.addEventListener('pointerout', this._handlePointerLeave.bind(this));
-		this._iframeDocument.body.addEventListener('pointerdown', this._handlePointerDown.bind(this), true);
-		this._iframeDocument.body.addEventListener('pointerup', this._handlePointerUp.bind(this));
-		this._iframeDocument.body.addEventListener('pointercancel', this._handlePointerUp.bind(this));
-		this._iframeDocument.body.addEventListener('pointermove', this._handlePointerMove.bind(this));
+		this._iframeDocument.addEventListener('pointerover', this._handlePointerOver.bind(this));
+		this._iframeDocument.addEventListener('pointerout', this._handlePointerLeave.bind(this));
+		this._iframeDocument.addEventListener('pointerdown', this._handlePointerDown.bind(this), true);
+		this._iframeDocument.addEventListener('pointerup', this._handlePointerUp.bind(this));
+		this._iframeDocument.addEventListener('pointercancel', this._handlePointerUp.bind(this));
+		this._iframeDocument.addEventListener('pointermove', this._handlePointerMove.bind(this));
 		this._iframeDocument.addEventListener('touchstart', this._handleTouchStart.bind(this));
 		this._iframeDocument.addEventListener('touchmove', this._handleTouchMove.bind(this), { passive: false });
 		this._iframeWindow.addEventListener('dragstart', this._handleDragStart.bind(this), { capture: true });
@@ -436,6 +436,9 @@ abstract class DOMView<State extends DOMViewState, Data> {
 		return this._getAnnotationFromRange(range, type, color);
 	}
 
+	/**
+	 * @returns Whether tool was used
+	 */
 	protected _tryUseTool() {
 		this._updateViewStats();
 
@@ -444,27 +447,31 @@ abstract class DOMView<State extends DOMViewState, Data> {
 				let selection = this._iframeWindow.getSelection();
 				if (selection && !selection.isCollapsed) {
 					this._openSelectionPopup(selection);
+					return true;
 				}
 			}
-			return;
 		}
-
-		if (this._tool.type == 'highlight' || this._tool.type == 'underline') {
+		else if (this._tool.type == 'highlight' || this._tool.type == 'underline') {
 			if (this._gotPointerUp) {
 				let annotation = this._touchAnnotationStartPosition
 					? this._previewAnnotation
 					: this._getAnnotationFromTextSelection(this._tool.type, this._tool.color);
-				if (annotation && annotation.text) {
-					this._options.onAddAnnotation(annotation);
-				}
 				this._iframeWindow.getSelection()?.removeAllRanges();
 				this._previewAnnotation = null;
+				this._renderAnnotations();
+
+				if (annotation?.text) {
+					this._options.onAddAnnotation(annotation);
+					return true;
+				}
 			}
 			else {
 				this._previewAnnotation = this._getAnnotationFromTextSelection(this._tool.type, this._tool.color);
+				this._renderAnnotations();
 			}
-			this._renderAnnotations();
 		}
+
+		return false;
 	}
 
 	protected _tryUseToolDebounced = debounce(this._tryUseTool.bind(this), 500);
@@ -1307,6 +1314,8 @@ abstract class DOMView<State extends DOMViewState, Data> {
 	}
 
 	private _handleAnnotationPointerDown = (id: string, event: React.PointerEvent) => {
+		event.stopPropagation();
+
 		// pointerdown handles:
 		//  - Selecting annotations when cycling isn't possible (no overlap between pointer and selected annotations)
 		//  - Opening the annotation context menu
@@ -1348,6 +1357,8 @@ abstract class DOMView<State extends DOMViewState, Data> {
 	};
 
 	private _handleAnnotationPointerUp = (id: string, event: React.PointerEvent) => {
+		event.stopPropagation();
+
 		// If pointerdown already performed an action due to this pointer, don't do anything
 		if (this._handledPointerIDs.has(event.pointerId)) {
 			this._handledPointerIDs.delete(event.pointerId);
@@ -1369,6 +1380,7 @@ abstract class DOMView<State extends DOMViewState, Data> {
 			this._openAnnotationPopup(this._annotationsByID.get(nextID)!);
 		}
 		this._lastKeyboardFocusedAnnotationID = null;
+		this._handledPointerIDs.add(event.pointerId);
 	};
 
 	private _getAnnotationsAtPoint(clientX: number, clientY: number): string[] {
@@ -1511,12 +1523,15 @@ abstract class DOMView<State extends DOMViewState, Data> {
 			// Deselect annotations when clicking outside the annotation layer
 			if (this._selectedAnnotationIDs.length) {
 				this._options.onSelectAnnotations([], event);
+				this._handledPointerIDs.add(event.pointerId);
 			}
 		}
 	}
 
 	protected _handlePointerUp(event: PointerEvent) {
-		if (!event.isPrimary) {
+		this._handledPointerIDs.delete(event.pointerId);
+
+		if (!event.isPrimary || event.defaultPrevented) {
 			return;
 		}
 
@@ -1532,9 +1547,17 @@ abstract class DOMView<State extends DOMViewState, Data> {
 			this._tryUseToolDebounced();
 		}
 		else {
-			this._tryUseTool();
+			let wasToolUsed = this._tryUseTool();
+			if (!wasToolUsed
+					&& !this._pointerMovedWhileDown
+					&& !this._handledPointerIDs.has(event.pointerId)
+					&& !(event.target as Element).closest('a')) {
+				this._options.onBackdropTap?.(event);
+			}
 		}
 		this._touchAnnotationStartPosition = null;
+		this._previewAnnotation = null;
+		this._renderAnnotations();
 		this._iframeDocument.body.classList.remove('creating-touch-annotation');
 	}
 
@@ -1953,6 +1976,7 @@ export type DOMViewOptions<State extends DOMViewState, Data> = {
 	onEPUBEncrypted: () => void;
 	onFocusAnnotation: (annotation: WADMAnnotation) => void;
 	onSetHiddenAnnotations: (ids: string[]) => void;
+	onBackdropTap?: (event: PointerEvent) => void;
 	getLocalizedString?: (name: string) => string;
 	data: Data & {
 		buf?: Uint8Array,

src/common/view.js
@@ -101,6 +101,7 @@ class View {
 			onKeyUp: nop,
 			onFocusAnnotation: nop,
 			onSetHiddenAnnotations: nop,
+			onBackdropTap: this._options.onBackdropTap,
 		};
 
 		if (this._type === 'epub') {

src/dom/epub/flow.ts
@@ -541,28 +541,28 @@ export class PaginatedFlow extends AbstractFlow {
 		this._sectionsContainer = this._iframeDocument.body.querySelector(':scope > .sections') as HTMLElement;
 		this._swipeIndicators = this._iframeDocument.querySelector('.swipe-indicators') as HTMLElement;
 
-		this._iframeDocument.addEventListener('keydown', this._handleKeyDown, { capture: true });
-		this._iframeDocument.addEventListener('pointerdown', this._handlePointerDown);
-		this._iframeDocument.addEventListener('pointermove', this._handlePointerMove);
-		this._iframeDocument.addEventListener('pointerup', this._handlePointerUp);
-		this._iframeDocument.addEventListener('pointerout', this._handlePointerCancel);
-		this._iframeDocument.addEventListener('pointercancel', this._handlePointerCancel);
-		this._iframeDocument.addEventListener('wheel', this._handleWheel, { passive: false });
-		this._iframeDocument.addEventListener('selectionchange', this._handleSelectionChange);
+		this._iframeDocument.body.addEventListener('keydown', this._handleKeyDown, { capture: true });
+		this._iframeDocument.body.addEventListener('pointerdown', this._handlePointerDown);
+		this._iframeDocument.body.addEventListener('pointermove', this._handlePointerMove);
+		this._iframeDocument.body.addEventListener('pointerup', this._handlePointerUp);
+		this._iframeDocument.body.addEventListener('pointerout', this._handlePointerCancel);
+		this._iframeDocument.body.addEventListener('pointercancel', this._handlePointerCancel);
+		this._iframeDocument.body.addEventListener('wheel', this._handleWheel, { passive: false });
+		this._iframeDocument.body.addEventListener('selectionchange', this._handleSelectionChange);
 		this._iframe.classList.add('flow-mode-paginated');
 		this._iframeDocument.body.classList.add('flow-mode-paginated');
 	}
 
 	override destroy(): void {
 		super.destroy();
-		this._iframeDocument.removeEventListener('keydown', this._handleKeyDown, { capture: true });
-		this._iframeDocument.removeEventListener('pointerdown', this._handlePointerDown);
-		this._iframeDocument.removeEventListener('pointermove', this._handlePointerMove);
-		this._iframeDocument.removeEventListener('pointerup', this._handlePointerUp);
-		this._iframeDocument.removeEventListener('pointerout', this._handlePointerCancel);
-		this._iframeDocument.removeEventListener('pointercancel', this._handlePointerCancel);
-		this._iframeDocument.removeEventListener('wheel', this._handleWheel);
-		this._iframeDocument.removeEventListener('selectionchange', this._handleSelectionChange);
+		this._iframeDocument.body.removeEventListener('keydown', this._handleKeyDown, { capture: true });
+		this._iframeDocument.body.removeEventListener('pointerdown', this._handlePointerDown);
+		this._iframeDocument.body.removeEventListener('pointermove', this._handlePointerMove);
+		this._iframeDocument.body.removeEventListener('pointerup', this._handlePointerUp);
+		this._iframeDocument.body.removeEventListener('pointerout', this._handlePointerCancel);
+		this._iframeDocument.body.removeEventListener('pointercancel', this._handlePointerCancel);
+		this._iframeDocument.body.removeEventListener('wheel', this._handleWheel);
+		this._iframeDocument.body.removeEventListener('selectionchange', this._handleSelectionChange);
 		this._iframe.classList.remove('flow-mode-paginated');
 		this._iframeDocument.body.classList.remove('flow-mode-paginated');
 	}
@@ -846,9 +846,11 @@ export class PaginatedFlow extends AbstractFlow {
 		let swipeAmount = (event.clientX - this._touchStartX) / PAGE_TURN_SWIPE_LENGTH_PX;
 		if (swipeAmount <= -1) {
 			this.navigateRight();
+			event.preventDefault();
 		}
 		else if (swipeAmount >= 1) {
 			this.navigateLeft();
+			event.preventDefault();
 		}
 		// If there's no selection, allow single-tap page turns
 		else if (this._iframeWindow.getSelection()!.isCollapsed
@@ -858,9 +860,11 @@ export class PaginatedFlow extends AbstractFlow {
 				&& !(event.target as Element).closest('a, .clickable-image')) {
 			if (event.clientX >= this._iframeWindow.innerWidth - PAGE_TURN_TAP_MARGIN_PX) {
 				this.navigateRight();
+				event.preventDefault();
 			}
 			else if (event.clientX <= PAGE_TURN_TAP_MARGIN_PX) {
 				this.navigateLeft();
+				event.preventDefault();
 			}
 		}
 	};

src/index.view-dev.js
@@ -64,7 +64,10 @@ window.createView = (options) => {
 		onChangeViewStats: (stats) => {
 			// Provides useful information about the view
 			console.log('View stats changed', stats);
-		}
+		},
+		onBackdropTap: (event) => {
+			console.log('Backdrop tap', event);
+		},
 	});
 
 	window._view = view;

commit 9581ca56ad2211902a953ff06ae182a5dbf65383

EPUB/Snapshot: Fix annotation cycling taking two clicks
Regression in 23575f6ed08908f84b0e4d64b3b9ef85dffe7c9e

src/dom/common/dom-view.tsx
@@ -1380,7 +1380,6 @@ abstract class DOMView<State extends DOMViewState, Data> {
 			this._openAnnotationPopup(this._annotationsByID.get(nextID)!);
 		}
 		this._lastKeyboardFocusedAnnotationID = null;
-		this._handledPointerIDs.add(event.pointerId);
 	};
 
 	private _getAnnotationsAtPoint(clientX: number, clientY: number): string[] {